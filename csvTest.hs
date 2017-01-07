be657d03c77bfb327e186a365c804be59b8a3707
Akagi <kaya3728@gmail>
2017-01-07 20:06:04 +0900

    success dealing utf-8 in cassava

--- csvTest.hs
+++ csvTest.hs
@@ -1,30 +1,32 @@
 {-# LANGUAGE OverloadedStrings #-}
 
-
+import Codec.Binary.UTF8.String
 import qualified Data.ByteString.Lazy as BL
 import Data.Csv
 import qualified Data.Vector as V
+import qualified Data.Text as T
+import qualified Data.Text.IO as T
+import qualified Data.Text.Encoding as T
 
 
-data Animal = Animal
+data Values = Values
             {   num :: !Int,
                 name :: !String,
                 what :: !String,
                 kg :: !Int
             }
 
-instance FromNamedRecord Animal where 
-        parseNamedRecord r = Animal <$>
+instance FromNamedRecord Values where 
+        parseNamedRecord r = Values <$>
                              r.: "num" <*> 
                              r.: "name" <*> 
                              r .: "what" <*> 
                              r .: "kg" 
 
 
-
 main = do
         csvData <- BL.readFile "testCsv.csv"
         case decodeByName csvData of
             Left err -> putStr err
-            Right (_, v) -> V.forM_ v $ \ p -> putStrLn $ show (num p) ++ name p ++ " is " ++ show (kg p) ++ " kg " ++ what p
+            Right (_, v) -> mapM_ putStrLn $ V.toList $  V.map name v
 
